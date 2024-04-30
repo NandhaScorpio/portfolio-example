1eba822642e5c863a7c6fca05dcd6fb6be4b41db
NandhaScorpio <gokulamstore2010@gmail.com>
2024-04-30 14:43:20 +0530

    Portfolio Website
    Hello

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".span", {
+    strings : ["Full Stack Developer","Mathematician","Freelancer"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+})
+
